1260f7a9498748ff53e3f9d0d646e5dd6f7e5241
Matej Zupanic <matejzupanic@gmail.com>
2018-03-22 23:59:07 +0100

    Api: - added Slim micro service to composer - basic implementation with single route and error handling

---
+++ api/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use MSISDNService\MSISDN;
+use MSISDNService\MnoRepository;
+
+require '../vendor/autoload.php';
+
+$app = new Slim\App();
+$container = $app->getContainer();
+
+// Set error handling
+$errorHandler = function ($c) {
+  return function ($request, $response, $error) use ($c) {
+    $r = new stdClass();
+    $r->Success = false;
+    $r->Message = $error->getMessage();
+
+    return $response->withStatus(500)
+      ->withHeader('Content-Type', 'application/json')
+      ->write(json_encode($r));
+  };
+};
+$container['errorHandler'] = $errorHandler;
+$container['phpErrorHandler'] = $errorHandler;
+
+$app->get('/v1/msisdn/{number}', function (Request $request, Response $response, array $args) {
+  $number = $args['number'];
+
+  $parser = MSISDN::getInstance();
+  $r = $parser->parseWithKeys($number);
+
+  return $response->withJson($r);
+});
+
+$app->run();
