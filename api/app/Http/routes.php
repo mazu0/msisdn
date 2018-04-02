1cd19a8f6bb6298bdd0656d1756735a5d489bd2b
Matej Zupanic <matejzupanic@gmail.com>
2018-04-03 00:37:13 +0200

    Msisdn service: registered to container

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -7,10 +7,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use MSISDNService\MSISDNService;
 
-// ToDo - write general handler for controller (when added)
-/*
-$app->addRoute(['GET', 'POST'], '/v2/{controller}[/{action}[/param]]', function (Request $request, Response $response, array $args) {
-});*/
+$app->map(['GET', 'POST'], '/v2/{controller}[/{action}/[{param}]]', function (Request $request, Response $response, $controller, $action = 'index', $params = '') use ($app) {
+  // resolve controller
+  $ctrlArr = explode('-', $controller);
+  $ctrlArr = array_map('ucfirst', $ctrlArr);
+  $controller = implode('', $ctrlArr);
+  $controller =  'Controllers\\' . $controller . 'Controller';
+
+  // get params
+  $args = array();
+  if (strlen($params) > 0) {
+    $params = explode('?', $params);
+    $params = $params[0];
+    $p = urldecode($params);
+    $args = explode('|', $p);
+  }
+
+  // invoke via reflection
+  $controllerInstance = new $controller($this);
+  $reflectionMethod = new ReflectionMethod($controller, $action);
+  $r = $reflectionMethod->invokeArgs($controllerInstance, $args);
+
+  return $response->withJson($r);
+});
 
 $app->map(['GET', 'POST'], '/v1/msisdn/[{msisdn}]', function (Request $request, Response $response, array $args) {
   $msisdn = null;
